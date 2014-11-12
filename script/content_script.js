117850c38a3478d440bbefb9b8bc6398e176732c
Karen <karenpenglabs@gmail.com>
2014-11-12 03:15:59 -0500

    i'm happily testing

---
+++ script/content_script.js
@@ -0,0 +1,9 @@
+//------------------DOC READY-------------------//
+var anchors = [];
+
+$("document").ready(function () {
+
+  //change cursor
+  //$("body").css("cursor", "url('" + chrome.extension.getURL('glitter_cursor.gif') + "'), default");
+
+});
